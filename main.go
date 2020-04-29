71e31cd75a689dc07adf41f72fd68a20f94c520d
Artem Artemyev <artem.artemiev@inspectorio.com>
2020-04-29 14:04:56 +0300

    First workable version

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	labels := []string{"dbcon", "state"}
+	psql_query_state := promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "psql_query_state",
+		Help: "psql query by user with state",
+	}, labels)
+	http.Handle("/metrics", promhttp.Handler())
+	go http.ListenAndServe(":2112", nil)
+	for {
+		for _, e := range os.Environ() {
+			envLine := strings.SplitN(e, "=", 2)
+			if strings.HasPrefix(envLine[0], "psql_") {
+				//				fmt.Println(envLine[0])
+				//				fmt.Println(envLine[1])
+				db, err := sql.Open("postgres", envLine[1])
+				if err != nil {
+					panic(err)
+				}
+				defer db.Close()
+				rows, err := db.Query("SELECT count(state),state FROM pg_stat_activity where state<>'idle' group by state;")
+				if err != nil {
+					print(err.Error())
+				}
+				defer rows.Close()
+				for rows.Next() {
+					var count int
+					var name string
+					err = rows.Scan(&count, &name)
+					if err != nil {
+						print(err.Error())
+					}
+					psql_query_state.WithLabelValues(envLine[0], name).Set(float64(count))
+					fmt.Println(name, count)
+				}
+			}
+		}
+		time.Sleep(60 * time.Second)
+	}
+}
